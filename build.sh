ef5c128a79d09954c53802a3862eeb0660bde0f2
Andoni Salcedo Navarro <andonisalcedo@gmail.com>
2024-02-09 12:52:08 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
-TAG="0.0.5"
+TAG="1.0.0"
 
 docker build . -t cloudmedialab/podinsights:$TAG
-
-docker tag cloudmedialab/podinsights:$TAG routerdi1315.uv.es:33443/cloudmedialab/podinsights:$TAG
-
-docker push routerdi1315.uv.es:33443/cloudmedialab/podinsights:$TAG
+docker push cloudmedialab/podinsights:$TAG
