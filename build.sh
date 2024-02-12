5268b0a7de2a38fd46dcf2c5833e789093c8a140
Andoni Salcedo Navarro <ansalna@alumni.uv.es>
2024-02-12 10:55:29 +0100

    to prod image

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 TAG="1.0.1"
 
-docker build . -t routerdi1315.uv.es:33443/cloudmedialab/podinsights:$TAG
-docker push routerdi1315.uv.es:33443/cloudmedialab/podinsights:$TAG
+docker build . -t cloudmedialab/podinsights:$TAG
+docker push cloudmedialab/podinsights:$TAG
